4be244cf8c78719fbbe21f34c8ea5c8553e5e0ef
courseragithubac <namita.kapoor@vfirst.com>
2016-11-23 22:46:05 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,54 +1,35 @@
 
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
-
-cachemean <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
-}
-
+# this function creates matrix
 makeCacheMatrix <- function(x = matrix()) {
+  #set the value of matrix.
+  # <<- operator is used to assign value to an object
   inv <- NULL
   set <- function(y){
     x <<- y
     m <<- NULL
   }
+  #get the value of function
   get <- function() x
   
-  
-  setinversemean <- function(mean) m <<- mean
+  #set the inverse
+  setinversemean <- function(inverse) m <<- inverse
   getinversemean <- function() m
   list(set = set, get = get, setinversemean = setinversemean, getinversemean = getinversemean)
 }
 
 
 cacheSolve <- function(x, ...) {
-
+#input return value of makeCacheMatrix
+#find inverse  
   inv <- x$getinversemean()
+  
+#if inverse is found then return value.
   if(!is.null(m)){
     return(m)
   }
   data <- x$get()
   m <- solve(data)
+  # set the value of inverse
   x$setinversemean(m)
   m      
 }
